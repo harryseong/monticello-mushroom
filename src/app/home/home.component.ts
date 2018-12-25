0a34e56d5089e1a9eaac18fa9d680c5fa8bd304c
Harry Seong <harry@northwestern.edu>
2018-12-24 18:20:48 -0600

    Updates to about page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('transitionAnimations', [
+      transition('* => fadeIn', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 })),
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+  transition = '';
 
   constructor() { }
 
   ngOnInit() {
+    this.transition = 'fadeIn';
   }
 
 }
--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+  styleUrls: ['./photos.component.css'],
+  animations: [
+    trigger('transitionAnimations', [
+      transition('* => fadeIn', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 })),
+      ])
+    ])
+  ]
 })
 export class PhotosComponent implements OnInit {
+  transition = '';
   imageSources: ['../src/assets/mushroom_shelves.jpg', '../src/assets/farm_flowers.jpg'];
 
   constructor() { }
 
   ngOnInit() {
+    this.transition = 'fadeIn';
   }
 
 }
